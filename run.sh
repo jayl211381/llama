7f8643790ea3a7342bcdd83740633b6e4d4ac854
Jay Li <jayl211381@gmail.com>
2023-12-31 14:07:43 -0500

    fixed run issue with windows

--- run.sh
+++ run.sh
@@ -1,2 +1,22 @@
-python -m torch.distributed.run --nproc_per_node 1 example_chat_completion.py \--ckpt_dir llama-2-7b-chat/ \--tokenizer_path tokenizer.model \--max_seq_len 512 --max_batch_size 6
+# python -m torch.distributed.run --nproc_per_node 1 example_chat_completion.py \--ckpt_dir llama-2-7b-chat/ \--tokenizer_path tokenizer.model \--max_seq_len 512 --max_batch_size 6
 
+# torchrun --nproc_per_node 1 example_chat_completion.py \--ckpt_dir llama-2-7b-chat/ \--tokenizer_path tokenizer.model \--max_seq_len 512 --max_batch_size 6
+
+# torchrun --nproc_per_node 1 example_chat_completion.py \
+#     --ckpt_dir llama-2-7b-chat/ \
+#     --tokenizer_path tokenizer.model \
+#     --max_seq_len 512 --max_batch_size 6
+
+torchrun --nproc_per_node 1 example_text_completion.py \
+    --ckpt_dir llama-2-7b/ \
+    --tokenizer_path tokenizer.model \
+    --max_seq_len 128 --max_batch_size 4
+
+# For git bash
+# conda activate c:/Users/Jayl2/OneDrive/llama/.conda
+
+# For path of torchrun 
+# https://stackoverflow.com/questions/77425569/llama2-running-pytorch-produces-a-failed-to-create-process
+
+# In generation.py, set nccl to gloo
+# "env": {"PL_TORCH_DISTRIBUTED_BACKEND":"gloo"},
--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 import torch
+import subprocess
+import os
+
+os.system("python --version")  
+# os.system("cmd 1 | another_command > output_file")  
+# subprocess.run(["ls", "-l"]) 
+subprocess.run(["python", "--version"]) 
 
 print(torch.cuda.is_available())
+
+print(torch.cuda.device_count())
+
+print(torch.cuda.current_device())
+
+print(torch.cuda.device(0))
+
+print(torch.cuda.get_device_name(0))
 
 print("done")
